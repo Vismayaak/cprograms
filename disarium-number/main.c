a046fc3bdd5f939320a42f206cce45afbdf78621
unknown <vismayakavungal00@gmail.com>
2024-07-29 23:07:13 +0530

    first commit

--- disarium-number/main.c
+++ disarium-number/main.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
-#include<math.h>
-int(int a)
-{
-    if(a>0)
-    return(pow(a%10,3)+disarium(a/10));
-}
-int main()
-{
-   int a;
-   printf("Enter a number:");
-   scanf("%d",&a);
-   if(disarium(a)==a){
-    printf("It is a disarium number");    
-   }
-   else{
-      printf("it is not an disarium number"); 
-   }
+#include <math.h>
+int main() {
+    int num, temp, digit, sum = 0, length = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    temp = num;
+    while (temp != 0) {
+        length++;
+        temp=temp/ 10;
+    }
+    temp = num;
+    while (temp != 0) {
+        digit = temp % 10;
+        sum =sum+ pow(digit, length);
+        temp =temp/ 10;
+        length--;
+    }
+    if (sum == num) {
+        printf("%d is a Disarium number\n", num);
+    } else {
+        printf("%d is not a Disarium number\n", num);
+    }
+
     return 0;
 }
